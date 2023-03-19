1b5d6e635dd5a53be7acc92652c61ff62716e73d
jangjonghwan <98254580+wkdwhdghks@users.noreply.github.com>
2023-03-19 14:57:25 +0900

    [Feat] 동작 기록 저장 구현

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,63 +1,13 @@
 import Square from "./Square";
 import "./Board.css";
-import { useState } from "react";
 
-export default function Board() {
-  const [squares, setSquares] = useState(Array(9).fill(null));
-  const [toggle, setToggle] = useState(true);
-
-  const calculateWinner = (squares) => {
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
-
-    for (let i = 0; i < lines.length; i++) {
-      const [a, b, c] = lines[i];
-      if (
-        squares[a] &&
-        squares[a] === squares[b] &&
-        squares[a] === squares[c]
-      ) {
-        return squares[a];
-      }
-    }
-    return null;
-  };
-
-  const winner = calculateWinner(squares);
-  let status;
-  if (winner) {
-    status = "winner: " + winner;
-  } else {
-    status = `Next player: ${toggle ? "X" : "O"}`;
-  }
-
-  const handleClick = (i) => {
-    let newSquares = [...squares];
-
-    if (calculateWinner(newSquares) || newSquares[i]) {
-      return;
-    }
-
-    newSquares[i] = toggle ? "X" : "O";
-    setSquares(newSquares);
-    setToggle((prev) => !prev);
-  };
-
+export default function Board({ squares, onClick }) {
   const renderSquare = (i) => {
-    return <Square value={squares[i]} onClick={() => handleClick(i)} />;
+    return <Square value={squares[i]} onClick={() => onClick(i)} />;
   };
 
   return (
     <div>
-      <div className="status">{status}</div>
       <div className="board-row">
         {renderSquare(0)}
         {renderSquare(1)}
